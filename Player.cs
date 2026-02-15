d8ba361a9d79e4683b87b908926290f0537c5c42
Mikkel Nielsen <mikkelwn97@gmail.com>
2026-02-15 19:56:55 +0100

    Can now play until one of the players hit target

--- Player.cs
+++ Player.cs
@@ -2,17 +2,24 @@ class Player
 {
     private string name;
     private int targetScore;
+    private int initialTargetScore;
     private int targetSets;
     private int targetLegs;
     
-    
+    private int currentSets;
+    private int currentLegs;
+  
 
-    public Player (string name, int targetScore, int targetSets, int targetLegs)
+
+    public Player (string name, int targetScore, int targetSets, int targetLegs, int  currentSets, int currentLegs, int initialTargetScore)
     {
         this.name = name;
+        this.initialTargetScore = targetScore;
         this.targetScore = targetScore;
         this.targetSets = targetSets;
         this.targetLegs = targetLegs;
+        this.currentSets = currentSets;
+        this.currentLegs = currentLegs;
     }
 
     public void SetupPlayer()
@@ -25,10 +32,12 @@ class Player
         if (int.TryParse(Console.ReadLine(),out int valueForTargetScore))
         {
             targetScore = valueForTargetScore;
+            initialTargetScore = valueForTargetScore;
         } else
         {
             Console.WriteLine("Not a valid number, please type a number");
             valueForTargetScore = int.Parse(Console.ReadLine());
+            initialTargetScore = valueForTargetScore;
         }
         targetScore = valueForTargetScore; 
         Console.Write("Enter the wanted number of Legs you would like to play: ");
@@ -78,6 +87,17 @@ class Player
 
     }
 
+    public int GetCurrentSets()
+    {
+        return currentSets;
+    }
+
+    public int GetCurrentLegs()
+    {
+        return currentLegs;
+    }
+    
+
     public string GetName()
     {
         return name;
@@ -94,12 +114,14 @@ class Player
 
     public void IncrementLegs()
     {
-        targetLegs++;
+        currentLegs++;
+        targetScore = initialTargetScore;
+        
     }
 
     public void IncrementSets()
     {
-        targetLegs++;
+        currentSets++;
     }
-  
+
 }
--- Program.cs
+++ Program.cs
@@ -2,17 +2,60 @@ using System.Runtime.Intrinsics.Arm;
 
 class Program
 {
-    Player player;
     public static void Main(string[] args)
     {
-        Player player1 = new Player("", 0,0,0);
+        Player player1 = new Player("", 0,0,0,0,0,0);
         player1.SetupPlayer();
 
-        Player player2 = new Player("", 0,0,0);
+        Player player2 = new Player("", 0,0,0,0,0,0);
         player2.SetupPlayer();
 
+        // Play until one player reaches target legs
+        while (player1.GetCurrentLegs() < player1.GetTargetLegs() && 
+               player2.GetCurrentLegs() < player2.GetTargetLegs())
+        {
+            // Player 1's turn
+            Console.WriteLine("\n--- " + player1.GetName() + "'s turn ---");
+            Console.WriteLine(player1.GetName() + " needs " + player1.GetTargetScore());
+            Console.Write("Enter score: ");
+            player1.NeededToWin();
+            
+            // Check if player1 won the leg
+            if (player1.GetTargetScore() == 0)
+            {
+                player1.IncrementLegs();
+                Console.WriteLine(player1.GetName() + " wins the leg! Score: " + 
+                                  player1.GetCurrentLegs() + "-" + player2.GetCurrentLegs());
                 
+                // Check if player1 won the match
+                if (player1.GetCurrentLegs() >= player1.GetTargetLegs())
+                {
+                    Console.WriteLine("\n🎯 " + player1.GetName() + " WINS THE MATCH! 🎯");
+                    break;
+                }
+                continue; // Skip player2's turn, start new leg
+            }
             
+            // Player 2's turn
+            Console.WriteLine("\n--- " + player2.GetName() + "'s turn ---");
+            Console.WriteLine(player2.GetName() + " needs " + player2.GetTargetScore());
+            Console.Write("Enter score: ");
+            player2.NeededToWin();
             
+            // Check if player2 won the leg
+            if (player2.GetTargetScore() == 0)
+            {
+                player2.IncrementLegs();
+                Console.WriteLine(player2.GetName() + " wins the leg! Score: " + 
+                                  player1.GetCurrentLegs() + "-" + player2.GetCurrentLegs());
+                
+                // Check if player2 won the match
+                if (player2.GetCurrentLegs() >= player2.GetTargetLegs())
+                {
+                    Console.WriteLine("\n🎯 " + player2.GetName() + " WINS THE MATCH! 🎯");
+                    break;
+                }
+            }
+        }
     }
 }
